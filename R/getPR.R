ded23a822dcee0cc564f6bef813d546573c4dc97
Loïc <dutri001@vanoise.(none)>
2013-06-24 16:18:30 +0200

    Slight problems with formatting of proj.4 expressions

--- R/getPR.R
+++ R/getPR.R
@@ -4,9 +4,12 @@ getPR <- function(country) {
   #Get the wrs grid
   data(wrs2) # wrs2
   
-  extm <- map("world", country, plot=FALSE, fill=TRUE, col='transparent', exact=TRUE) #For some reason the fill and col arguments seem to be important ...
+  extm <- map("worldHires", country, plot=FALSE, fill=TRUE, col='transparent', exact=TRUE) #For some reason the fill and col arguments seem to be important ...
   IDs <- sapply(strsplit(extm$names, ":"), function(x) x[1])
   exts <- map2SpatialPolygons(extm, IDs=IDs, proj4string=CRS("+proj=latlong +datum=WGS84"))
+#   if(!identical(proj4string(exts), proj4string(wrs2))) {
+#       exts <- spTransform(exts, CRS(proj4string(wrs2)))
+#   }
   
   # Perform the Spatial Query'
   x <- wrs2[exts,]
