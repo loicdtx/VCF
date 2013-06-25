a1c4b8b50e3b83d6765ce03c513043362089784f
Loïc <dutri001@vanoise.(none)>
2013-06-25 16:19:53 +0200

    Small bug fix for the download function

--- R/downloadPR.R
+++ R/downloadPR.R
@@ -5,6 +5,7 @@ downloadPR <- function(pr, year, dir, log=NULL, baseURL='ftp://ftp.glcf.umd.edu/
   if (is.list(pr)) { # Assuming the list provided is the variable returned by getPR() function
     pr <- pr$PR
   }
+  pr = sprintf('%06d', pr) # Make the pr individual objects always 6 digits
   
   dir.create(dir, showWarnings = FALSE)
   if(is.null(log)) {
@@ -13,13 +14,13 @@ downloadPR <- function(pr, year, dir, log=NULL, baseURL='ftp://ftp.glcf.umd.edu/
   cat(date(), file=log, sep="\n") #First line of the log file
   
   nbFiles <- length(pr) * length(year)
-  print(sprintf('About to start downloading \n %d to download in total', nbFiles))
+  print(sprintf('About to start downloading: %d files to download in total', nbFiles))
   
-  dl <- function(x, y) { # y is year
+  dl <- function(x, y) { # y is year ; x is the pr element and has already been converted to character
     
     # Build URL
-    p <- substr(as.character(x),1,3)
-    r <- substr(as.character(x),4,6)
+    p <- substr(x,1,3)
+    r <- substr(x,4,6)
     urlP <- sprintf('p%s/r%s/p%sr%s_TC_%d/', p, r, p, r, y) #Path part of the url
     urlF <- sprintf('p%sr%s_TC_%d.tif.gz', p, r, y) # Filename part of the url
     url <- sprintf('%s%s%s', baseURL, urlP, urlF)
@@ -55,6 +56,8 @@ downloadPR <- function(pr, year, dir, log=NULL, baseURL='ftp://ftp.glcf.umd.edu/
     return(tryReport)
   }
   
-  lapply(X=year, FUN=sapply(X=pr, FUN=fun))  
+  for (i in year) {
+    sapply(X=pr, FUN=fun, i)
+  }
   
 }
